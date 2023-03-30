3a71facbf57cbea9a1b6723a4604059e25b9a96c
Nenad Ilic <nenadilic84@gmail.com>
2023-03-30 13:21:15 +0000

    Inital fixes

--- examples/nomad/nomad-docker-pub/app.py
+++ examples/nomad/nomad-docker-pub/app.py
@@ -7,22 +7,41 @@ import os
 import awsiot.greengrasscoreipc
 import awsiot.greengrasscoreipc.model as model
 
+
+NOMAD_SHORT_ALLOC_ID = os.getenv('NOMAD_SHORT_ALLOC_ID')
+
+def get_used_mem():
+    with open('/proc/meminfo', 'r') as f:
+        for line in f:
+            if line.startswith('MemTotal:'):
+                total_mem = int(line.split()[1]) * 1024  # convert to bytes
+            elif line.startswith('MemAvailable:'):
+                available_mem = int(line.split()[1]) * 1024  # convert to bytes
+                break
+
+    return total_mem - available_mem
+
+def get_cpu_usage():
+    with open('/proc/stat', 'r') as f:
+        line = f.readline()
+        cpu_time = sum(map(int, line.split()[1:]))
+        idle_time = int(line.split()[4])
+
+    return (cpu_time - idle_time) / cpu_time
+
 if __name__ == '__main__':
     ipc_client = awsiot.greengrasscoreipc.connect()
 
     while True:
         telemetry_data = {
             "timestamp": int(round(time.time() * 1000)),
-            "battery_state_of_charge": 42.5,
-            "location": {
-                "longitude": 48.15743,
-                "latitude": 11.57549,
-            },
+            "used_memory": get_used_mem(),
+            "cpu_usage": get_cpu_usage()
         }
 
         op = ipc_client.new_publish_to_iot_core()
         op.activate(model.PublishToIoTCoreRequest(
-            topic_name="my/iot/telemetry",
+            topic_name=f"{NOMAD_SHORT_ALLOC_ID}/iot/telemetry",
             qos=model.QOS.AT_LEAST_ONCE,
             payload=json.dumps(telemetry_data).encode(),
         ))
